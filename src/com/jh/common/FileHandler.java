8c14ad4b71bdf6653b5ef461822829b354d1d66d
KaKaHe <becker_heo@hotmail.com>
2017-07-04 15:10:59 -0400

    Initialize

---
+++ src/com/jh/common/FileHandler.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.jh.common;
+
+import java.io.*;
+
+/**
+ * @author jiah
+ *
+ */
+public class FileHandler implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5008034905485871450L;
+	
+	private static FileHandler instance = null;
+	
+	/**
+	 * 
+	 */
+	private FileHandler(){
+		//Singleton constructor
+	}
+	
+	/**
+	 * @return	object instance
+	 */
+	public static FileHandler getInstance(){
+		if(instance == null){
+			instance = new FileHandler();
+		}
+		return instance;
+	}
+
+}
---
+++ src/com/jh/datastorage/DataStorage.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jh.datastorage;
+
+/**
+ * @author jiah
+ *
+ */
+public class DataStorage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
